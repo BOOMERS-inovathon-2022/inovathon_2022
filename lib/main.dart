ca2c72d5f1d55a5543d0a025ce0f5cce35a67f06
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-10-12 13:25:53 -0300

    Configuração Firebase API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:inovathon_2022/ui/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
