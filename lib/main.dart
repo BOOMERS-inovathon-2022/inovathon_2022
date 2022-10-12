1d96d93ceea96ffc1745359aa330d5de7564b62b
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-10-12 11:44:50 -0300

    Alteração MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:inovathon_2022/ui/screen/home/home_page.dart';
+import 'package:inovathon_2022/ui/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Inovathon',
-      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
-      home: const HomePage(title: 'Inovathon 2022'),
-    );
-  }
 }
---
+++ lib/ui/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:inovathon_2022/ui/screen/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Inovathon',
+      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
+      home: const HomePage(title: 'Inovathon 2022'),
+    );
+  }
+}
