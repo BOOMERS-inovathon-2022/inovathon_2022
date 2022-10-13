699ff26a909ea471a66baefddc8785c1f86153be
Andrey <andreybr2002@hotmail.com>
2022-10-13 08:38:44 -0300

    Cadastro de endereço Iniciando implementação para cadastro de endereço do cliente

---
+++ lib/ui/screen/address_registration/address_registration_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:inovathon_2022/ui/screen/address_registration/address_registration_page.dart';
+
+import '../../shared/shared_input.dart';
+
+class AddressRegistrationWidget extends State<AddressRegistrationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Cadastro de Endereço',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.only(right: 30),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 10),
+                SharedTextInput(
+                  keyboardType: TextInputType.text,
+                  hintText: 'Logradouro',
+                ),
+                const SizedBox(height: 10),
+                SharedTextInput(
+                  keyboardType: TextInputType.text,
+                  hintText: 'Bairro',
+                ),
+                const SizedBox(height: 10),
+                SharedTextInput(
+                  keyboardType: TextInputType.number,
+                  hintText: 'Número',
+                ),
+                const SizedBox(height: 10),
+                SharedTextInput(
+                  keyboardType: TextInputType.number,
+                  hintText: 'CEP',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
