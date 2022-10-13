078ce279fb46320a6c9fe47cb870f1354407baa6
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-10-13 08:29:20 -0300

    Fixes & Changes Photo Carousel
    Nova dependencia para controle das fotos do Carrossel de fotos do profile do Produtos

--- lib/ui/screen/seller_profile/seller_details/seller_details_widget.dart
+++ lib/ui/screen/seller_profile/seller_details/seller_details_widget.dart
@@ -1,35 +1,46 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:inovathon_2022/ui/screen/seller_profile/components/title_divider.dart';
 import 'package:inovathon_2022/ui/screen/seller_profile/seller_details/seller_details_page.dart';
 import 'package:inovathon_2022/ui/theme/colors.dart';
 
 class SellerDetailsWidget extends State<SellerDetailsPage> {
-  List<String> productsImages = [
-    "https://imagensemoldes.com.br/wp-content/uploads/2020/07/Cenoura-PNG.png",
-    "https://images.squarespace-cdn.com/content/v1/5b8edfa12714e508f756f481/1543944726778-3R28J0BST06GRZCOF7UR/alface-crespa-verde-hidropônica.png",
-    "https://imagensemoldes.com.br/wp-content/uploads/2020/03/Foto-de-Batata-Grande-PNG-1024x911.png"
+  List<List<String>> productsImages = [
+    ["https://imagensemoldes.com.br/wp-content/uploads/2020/07/Cenoura-PNG.png", "Cenoura"],
+    [
+      "https://images.squarespace-cdn.com/content/v1/5b8edfa12714e508f756f481/1543944726778-3R28J0BST06GRZCOF7UR/alface-crespa-verde-hidropônica.png",
+      "Alface"
+    ],
+    [
+      "https://imagensemoldes.com.br/wp-content/uploads/2020/03/Foto-de-Batata-Grande-PNG-1024x911.png",
+      "Batata"
+    ],
+    [
+      "https://iguabagrande.tinocosupermercados.com.br/wp-content/uploads/2022/04/642.png",
+      "Mandioca"
+    ],
   ];
   List<String> productionImages = [
     "https://static.todamateria.com.br/upload/pe/ss/pessoascultivando-cke.jpg",
     "https://blog.belagro.com.br/content/uploads/2020/10/importância-da-agricultura-familiar-750x422.jpg",
-    "https://www.camara.leg.br/midias/image/2021/12/img20210519120415306-1-768x512.jpeg"
+    "https://www.camara.leg.br/midias/image/2021/12/img20210519120415306-1-768x512.jpeg",
+    "https://blog4.mfrural.com.br/wp-content/uploads/2022/01/tarefas-familia.jpg",
   ];
 
-  late PageController _pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(viewportFraction: 0.5);
-  }
+  var carouselOptions = CarouselOptions(
+    height: 200,
+    aspectRatio: 16 / 9,
+    viewportFraction: 0.8,
+    initialPage: 0,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
+    return ListView(
+      // mainAxisSize: MainAxisSize.min,
       children: [
         const Padding(
-          padding: EdgeInsets.only(left: 10),
+          padding: EdgeInsets.only(left: 10, top: 10),
           child: TitleDivider(
             title: "Quem sou",
             endDivider: 270,
@@ -50,50 +61,53 @@ class SellerDetailsWidget extends State<SellerDetailsPage> {
             endDivider: 240,
           ),
         ),
-        Flexible(
-          child: PageView.builder(
-              itemCount: productsImages.length,
-              pageSnapping: false,
-              controller: _pageController,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.only(right: 20),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: kBorderColor),
-                    borderRadius: BorderRadius.circular(20),
+        CarouselSlider.builder(
+          itemCount: productsImages.length,
+          options: CarouselOptions(
+            height: 100,
+            aspectRatio: 16 / 9,
+            viewportFraction: 0.4,
+            initialPage: 0,
+          ),
+          itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) => Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: kBorderColor),
+              borderRadius: BorderRadius.circular(20),
+              // borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      // fit: BoxFit.fitHeight,
+                      image: NetworkImage(
+                        productsImages[itemIndex][0],
+                      ),
+                    )),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                            // fit: BoxFit.fitHeight,
-                            image: NetworkImage(
-                              productsImages[index],
-                            ),
-                          )),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            "Alface",
-                            textAlign: TextAlign.start,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                    ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      productsImages[itemIndex][1],
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
-                );
-              }),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+          ),
         ),
         const SizedBox(height: 25),
         const Padding(
@@ -103,26 +117,22 @@ class SellerDetailsWidget extends State<SellerDetailsPage> {
             endDivider: 250,
           ),
         ),
-        Expanded(
-          child: PageView.builder(
-              itemCount: productionImages.length,
-              pageSnapping: false,
-              controller: _pageController,
-              itemBuilder: (context, index) {
-                return Container(
-                    // width: 60,
-                    // height: 100,
-                    margin: const EdgeInsets.only(right: 20),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: NetworkImage(
-                            productionImages[index],
-                          ),
-                        )));
-              }),
-        ),
+        CarouselSlider.builder(
+          itemCount: productionImages.length,
+          options: carouselOptions,
+          itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) => Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    productionImages[itemIndex],
+                  ),
+                )),
+          ),
+        )
       ],
     );
   }
